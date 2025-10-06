425101a9934923bf2b05f007780573a93a272260
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-10-05 22:14:44 -0500

    Use new sample file for testing

--- main.go
+++ main.go
@@ -8,19 +8,34 @@ import (
 )
 
 func main() {
-	htmlFile, err := os.Open("samples/test.html")
+	htmlFile, err := os.Open("samples/sample_list.html")
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 	
-	htmlNode, err := html.Parse(htmlFile)
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
+	depth := 0
+	z := html.NewTokenizer(htmlFile)
+	for {
+		tt := z.Next()
+		switch tt {
+		case html.ErrorToken:
+			fmt.Println(z.Err().Error())
+			return
+		case html.TextToken:
+			if depth > 0 {
+				fmt.Println(string(z.Text()))
+			}
+		case html.StartTagToken, html.EndTagToken:
+			tn, _ := z.TagName()
+			if len(tn) == 1 && tn[0] == 'a' {
+				if tt == html.StartTagToken {
+					depth++
+				} else {
+					depth--
+				}
+			}
+		}
 	}
-	for n := range htmlNode.Descendants() {
-		fmt.Printf("%#v\n", n.Data)
-	}
-	os.Exit(0)
+
 }
