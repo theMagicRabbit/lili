e3ed1c0df2a5db86083181d69dc4da1f56145d36
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-10-05 21:30:15 -0500

    Add some test parsing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/theMagicRabbit/lili
 
 go 1.25.1
+
+require golang.org/x/net v0.44.0 // indirect
--- main.go
+++ main.go
@@ -1,4 +1,24 @@
-package lili
+package main
+
+import (
+	"fmt"
+	"os"
+	
+	"golang.org/x/net/html"
+)
 
 func main() {
+	htmlFile, err := os.Open("samples/test.html")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	
+	htmlNode, err := html.Parse(htmlFile)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("%#vPrintln\n", htmlNode)
+	os.Exit(0)
 }
